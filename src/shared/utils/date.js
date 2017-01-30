8b5c3cfa73d11bd28776019caf5b302812e9b569
Sasha Klein <sashafklein@gmail.com>
2017-01-29 16:36:58 -0800

    Fixed tests

--- src/shared/utils/date.js
+++ src/shared/utils/date.js
@@ -1,3 +1,3 @@
 export const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-export const isDateElement = el => MONTHS.includes(el) || (el !== undefined && !isNaN(el.replace(/[,.;]/, '')));
+export const isDateElement = el => MONTHS.indexOf(el) !== -1 || (el !== undefined && !isNaN(el.replace(/[,.;]/, '')));
