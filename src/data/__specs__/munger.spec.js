5f88b021a774af0f5ad47465285d67f7eb9d5ad7
Sasha Klein <sashafklein@gmail.com>
2017-02-15 20:12:56 -0800

    Munger spec looking good

--- src/data/__specs__/munger.spec.js
+++ src/data/__specs__/munger.spec.js
@@ -2,7 +2,6 @@
 
 import { fromJS } from 'immutable';
 import startingData from '../sample';
-import _ from 'lodash';
 
 import { stitchTogetherContests } from '../munger';
 
@@ -10,6 +9,27 @@ const allData = fromJS(startingData.ElectionReport);
 const contests = allData.getIn(['Election', 'ContestCollection', 'Contest']);
 const parties = allData.getIn(['PartyCollection', 'Party']);
 const candidates = allData.getIn(['Election', 'CandidateCollection', 'Candidate']);
+
+const assertAttr = (obj, attr) => {
+  if (!obj.get(attr)) {
+    console.error(`Expected obj to have attr.\n Obj: ${obj}\n Attr: ${attr}`);
+    expect(obj.get(attr)).to.exist;
+  }
+};
+
+const runContestTest = (contest, contestAttrs, optionAttrs, mandateOptions = true) => {
+  contestAttrs.forEach(attr => assertAttr(contest, attr));
+
+  const firstOption = contest.getIn(['options', 0]);
+  if (firstOption) {
+    optionAttrs.forEach(attr => assertAttr(firstOption, attr));
+  } else if (mandateOptions) {
+    console.error(contest);
+    throw new Error('Expected contest to have options, but had none.');
+  } else {
+    console.error('Contest found without options:', contest);
+  }
+};
 
 describe('stitchTogetherContests', () => {
   let stitched;
@@ -19,47 +39,37 @@ describe('stitchTogetherContests', () => {
   });
 
   it('handles candidate contests correctly', () => {
-    const contest = stitched.find(c => c.get('type') === 'CandidateContest');
-    expect(contest.get('name')).to.exist;
-    expect(contest.get('id')).to.exist;
-    expect(contest.getIn(['options', 0, 'name'])).to.exist;
-    expect(contest.getIn(['options', 0, 'partyName'])).to.exist;
-    expect(contest.getIn(['options', 0, 'partyID'])).to.exist;
-    expect(contest.getIn(['options', 0, 'id'])).to.exist;
+    stitched.filter(c => c.get('type') === 'CandidateContest').forEach(contest => {
+      runContestTest(
+        contest,
+        ['name', 'id', 'voteLimit'],
+        ['name', 'partyName', 'partyID', 'id'],
+        false
+      );
+    });
   });
 
-  // it('handles ballot measure contests correctly', () => {
+  it('handles ballot measure contests correctly', () => {
+    stitched.filter(c => c.get('type') === 'BallotMeasureContest').forEach(contest => {
+      runContestTest(
+        contest,
+        ['name', 'electoralDistrictID', 'text', 'id', 'voteLimit'],
+        ['name', 'id']
+      );
+    });
+  });
 
-  // });
+  it('handles party contests correctly', () => {
+    stitched.filter(c => c.get('type') === 'PartyContest').forEach(contest => {
+      runContestTest(
+        contest,
+        ['type', 'name', 'voteLimit', 'electoralDistrictID', 'id'],
+        ['name', 'abbreviation', 'id']
+      );
+    });
+  });
 
-  // it('handles party contests correctly', () => {
-
-  // });
-
-  // it('handles all incoming data', () => {
-  //   expect(contests.size).not.to.eq(0);
-  //   expect(parties.size).not.to.eq(0);
-  //   expect(candidates.size).not.to.eq(0);
-
-  //   expect(
-  //     stitched.filter(r => r.get('name') && r.get('type') && r.get('id')).size
-  //   ).to.eq(contests.size);
-  //   // result.forEach(contest => {
-  //   //   expect(
-  //   //     ['PartyContest', 'CandidateContest', 'BallotMeasureContest']
-  //   //       .includes(contest.get('type'))
-  //   //   ).to.eq(true);
-
-  //   //   expect(contest.get('voteLimit')).to.exist;
-
-  //   //   contest.get('options').forEach(option => {
-  //   //     expect(option.get('id')).to.exist;
-  //   //     expect(option.get('name')).to.exist;
-  //   //   });
-  //   // });
-
-  //   // const candidateContests = result.filter(c => c.get('type') === 'CandidateContest');
-  //   // const candidateParties = candidateContests.map(c => c.get('partyName'));
-  //   // expect(_.uniq(candidateParties).sort()).to.eq(['Democratic', 'Green', 'Libertarian', 'Republican']);
-  // });
+  it('does not lose anything', () => {
+    expect(stitched.size).to.eq(contests.size);
+  });
 });
--- src/data/munger.js
+++ src/data/munger.js
@@ -5,21 +5,6 @@ import { Map, fromJS } from 'immutable';
 const partyNamePath = ['Name', 'Text', '__text'];
 const candidateNamePath = ['BallotName', 'Text', '__text'];
 
-// const incorporateExtra = (originalObject, extrasObject, immutableObject) => fromJS(
-//   Object.assign(
-//     {},
-//     originalObject,
-//     Object.keys(extrasObject)
-//       .reduce((obj, key) => Object.assign(
-//         {},
-//         obj,
-//         { [key]: immutableObject.getIn(extrasObject[key]) }
-//       ), {})
-//   )
-// );
-
-const log = (thing) => {  return thing; };
-
 const optionFromSelection = (selection, list, selectionNamePath, objectPath, optionExtra) => {
   if (!selection.getIn) {
     return null; // TODO figure out how to handle bad data like candidate-62 contest
@@ -37,36 +22,15 @@ const optionFromSelection = (selection, list, selectionNamePath, objectPath, opt
     ));
     return objectJSON;
   } else {
-    console.log('No associated object found for', selection.toJS());
+    console.log(
+      'No associated object found for',
+      selection.toJS(),
+      'Searching in',
+      objectPath
+    );
     return null;
   }
 };
-
-// const createStitchedList = (contest, list, selectionNamePath, contestExtra, optionExtra = {}) => {
-//   let selections = contest.get('BallotSelection');
-//   const type = contest.get('_xsi:type');
-//   if (!selections) {
-//     return null; // TODO - Handle weird data like 'PRECINCT 3, DIRECTOR, BARTON SPRINGS EDWARDS AQUIFER CONSERVATION DISTRICT'
-//   } else if (selections instanceof Map) {
-//     // Some selections data is a single object, instead of an array of them
-//     selections = fromJS([selections]);
-//   }
-
-//   return fromJS(Object.assign({},
-//     {
-//       options: selections.map(selection => optionFromSelection(
-//         selection,
-//         list,
-//         selectionNamePath,
-//         type === 'PartyContest' ? ['PartyId'] : ['CandidateId', 0]
-//       )).filter(option => option),
-//       type,
-//       name: contest.get('Name'),
-//       voteLimit: parseInt(contest.get('NumberElected') || 1)
-//     },
-//     contestExtra
-//   ));
-// };
 
 // TODO - Handle weird data like 'PRECINCT 3, DIRECTOR, BARTON SPRINGS EDWARDS AQUIFER CONSERVATION DISTRICT'
 const getSelections = contest => {
@@ -79,19 +43,20 @@ const getSelections = contest => {
 };
 
 const createPartyContestList = (contest, parties) => {
-  return log(fromJS({
+  return fromJS({
     options: getSelections(contest).map(selection => optionFromSelection(
       selection,
       parties,
       partyNamePath,
-      ['CandidateId', 0],
+      ['PartyId'],
       {}
     )).filter(option => option),
     type: contest.get('_xsi:type'),
     name: contest.get('Name'),
+    id: contest.get('_objectId'),
     voteLimit: parseInt(contest.get('NumberElected') || 1),
     electoralDistrictID: contest.get('ElectoralDistrictId')
-  }));
+  });
 };
 
 
@@ -102,11 +67,11 @@ const createCandidateContestList = (contest, candidates, parties) => {
 
     return {
       partyID: candidate.get('PartyId'),
-      partyName: party && party.getIn(partyNamePath)
+      partyName: (party && party.getIn(partyNamePath)) || 'No Party'
     };
   };
 
-  return log(fromJS({
+  return fromJS({
     options: getSelections(contest).map(selection => optionFromSelection(
       selection,
       candidates,
@@ -118,7 +83,7 @@ const createCandidateContestList = (contest, candidates, parties) => {
     type: contest.get('_xsi:type'),
     name: contest.get('Name'),
     voteLimit: parseInt(contest.get('NumberElected') || 1)
-  }));
+  });
 };
 
 const createBallotMeatureContestList = contest => fromJS({
