32e2009d668019eb16e917346d4c070a98426a05
Sasha Klein <sashafklein@gmail.com>
2017-02-19 14:49:42 -0800

    Added basic test

--- src/bundles/ReviewVotes/__specs__/index.spec.js
+++ src/bundles/ReviewVotes/__specs__/index.spec.js
@@ -1,11 +1,26 @@
-/* eslint no-undef:0 */
+/* eslint no-undef:0, no-unused-expressions:0 */
 
-import { spy } from 'sinon';
+import { Map, List } from 'immutable';
+import { ListView } from 'react-native';
 
 import { ReviewVotes } from '../';
+import PageWithActions from '../../../shared/components/PageWithActions';
 
 describe('<ReviewVotes />', () => {
   it('has a test', () => {
-    expect(true).to.eq(false);
+    const comp = mockComp(ReviewVotes, {
+      contests: List([
+        { id: 'one', name: 'Here is a name' },
+        { id: 'two', name: 'Here is another' }
+      ]),
+      selections: Map({})
+    });
+
+    const page = comp.find(PageWithActions).get(0);
+    expect(page).to.exist;
+    expect(page.props.headerItems.map(i => i.content)).to.eql(['Settings', 'Help']);
+
+    const list = comp.find(ListView).get(0);
+    expect(list).to.exist;
   });
 });
